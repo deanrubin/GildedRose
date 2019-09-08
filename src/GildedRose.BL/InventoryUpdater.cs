d77f678ef82c5cd89340484aaa3cf5de331b497c
Dean Rubin <deanrubin@gmail.com>
2019-09-08 19:25:17 +0300

    Add more test, add validations for types

--- src/GildedRose.BL/InventoryUpdater.cs
+++ src/GildedRose.BL/InventoryUpdater.cs
@@ -7,7 +7,7 @@ namespace GildedRose.BL
 {
     public class InventoryUpdater
     {
-        private readonly IDictionary<ItemForSellType, Action<ItemForSell>> _updateQualityFunctions = new Dictionary<ItemForSellType, Action<ItemForSell>>()
+        private readonly IDictionary<ItemForSellType, Action<ItemForSell>> _updateQualityActions = new Dictionary<ItemForSellType, Action<ItemForSell>>()
         {
             {ItemForSellType.Normal, UpdateQualityOfNormalItem},
             {ItemForSellType.BackstagePasses, UpdateQualityOfBackstagePassesItem},
@@ -18,50 +18,63 @@ namespace GildedRose.BL
 
         public Task Update(ItemForSell itemToUpdate)
         {
-            if (!_updateQualityFunctions.TryGetValue(itemToUpdate.Type, out var func))
+            if (!_updateQualityActions.TryGetValue(itemToUpdate.Type, out var action))
             {
                 throw new ArgumentException($"The item with the name: {itemToUpdate.Name} and the type: {itemToUpdate.Type} is invalid");
             }
 
-            func.Invoke(itemToUpdate);
+            action.Invoke(itemToUpdate);
+            UpdateSellIn(itemToUpdate);
+            return Task.CompletedTask;
+        }
 
+        private static void UpdateSellIn(Item itemToUpdate)
+        {
             itemToUpdate.SellIn--;
             if (itemToUpdate.SellIn < 0)
             {
                 itemToUpdate.SellIn = 0;
             }
-
-            return Task.CompletedTask;
         }
 
         private static void UpdateQualityOfAgedBrieItem(ItemForSell itemToUpdate)
         {
+            if (itemToUpdate.Type != ItemForSellType.AgedBrie)
+            {
+                throw new ArgumentException("The function: UpdateQualityOfAgedBrieItem can only accept ItemForSell from type: AgedBrie");
+            }
+
             itemToUpdate.Quality++;
             itemToUpdate.Quality = Math.Min(itemToUpdate.Quality, 50);
         }
 
         private static void UpdateQualityOfConjuredItem(ItemForSell itemToUpdate)
         {
-            itemToUpdate.Quality--;
-            UpdateQualityOfNormalItem(itemToUpdate);
+            if (itemToUpdate.Type != ItemForSellType.Conjured)
+            {
+                throw new ArgumentException("The function: UpdateQualityOfConjuredItem can only accept ItemForSell from type: Conjured");
+            }
+
+            UpdateQualityByValue(itemToUpdate, 2);
         }
 
         private static void UpdateQualityOfNormalItem(ItemForSell itemToUpdate)
         {
-            itemToUpdate.Quality--;
-            if (itemToUpdate.SellIn <= 0)
+            if (itemToUpdate.Type != ItemForSellType.Normal)
             {
-                itemToUpdate.Quality--;
+                throw new ArgumentException("The function: UpdateQualityOfNormalItem can only accept ItemForSell from type: Normal");
             }
 
-            if (itemToUpdate.Quality < 0)
-            {
-                itemToUpdate.Quality = 0;
-            }
+            UpdateQualityByValue(itemToUpdate, 1);
         }
 
-        private static void UpdateQualityOfBackstagePassesItem(ItemForSell itemToUpdate)
+        private static void UpdateQualityOfBackstagePassesItem(ItemForSell itemToUpdate) 
         {
+            if (itemToUpdate.Type != ItemForSellType.BackstagePasses)
+            {
+                throw new ArgumentException("The function: UpdateQualityOfBackstagePassesItem can only accept ItemForSell from type: BackstagePasses");
+            }
+
             if (itemToUpdate.SellIn <= 0)
             {
                 itemToUpdate.Quality = 0;
@@ -81,6 +94,20 @@ namespace GildedRose.BL
             }
 
             itemToUpdate.Quality = Math.Min(itemToUpdate.Quality, 50);
+        }
+
+        private static void UpdateQualityByValue(Item itemToUpdate, int value)
+        {
+            itemToUpdate.Quality = itemToUpdate.Quality - value;
+            if (itemToUpdate.SellIn <= 0)
+            {
+                itemToUpdate.Quality = itemToUpdate.Quality - value;
+            }
+
+            if (itemToUpdate.Quality < 0)
+            {
+                itemToUpdate.Quality = 0;
+            }
         }
     }
 }
--- src/GildedRose.Tests/InventoryUpdaterTest.cs
+++ src/GildedRose.Tests/InventoryUpdaterTest.cs
@@ -25,6 +25,19 @@ namespace GildedRose.Tests
         }
 
         [Theory]
+        [InlineData(0, 30)]
+        [InlineData(0, 0)]
+        public async Task UpdateNormalItemSellInPassedTest_UpdateNormalItem_QualityDecreaseByTwo(int sellIn, int quality)
+        {
+            var item = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.Normal };
+            await _inventoryUpdater.Update(item);
+            var expQuality = Math.Max(quality - 2, 0);
+            var expSellIn = Math.Max(sellIn - 1, 0);
+            Assert.Equal(expQuality, item.Quality);
+            Assert.Equal(expSellIn, item.SellIn);
+        }
+
+        [Theory]
         [InlineData(20, 30)]
         [InlineData(20, 50)]
         [InlineData(20, 0)]
@@ -61,12 +74,24 @@ namespace GildedRose.Tests
         }
 
         [Theory]
+        [InlineData(0, 30)]
+        [InlineData(0, 50)]
+        [InlineData(0, 0)]
+        public async Task UpdateBackstagePassesItemWhenSellInPassedTest_UpdateItem_QualityEqualsZero(int sellIn, int quality)
+        {
+            var backstagePassesItem = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.BackstagePasses };
+            await _inventoryUpdater.Update(backstagePassesItem);
+            Assert.Equal(0, backstagePassesItem.Quality);
+        }
+
+        [Theory]
         [InlineData(10, 30)]
         [InlineData(5, 50)]
         [InlineData(5, 30)]
         [InlineData(10, 0)]
         [InlineData(5, 0)]
-        [InlineData(20, 0)]
+        [InlineData(0, 0)]
+        [InlineData(0, 20)]
         public async Task UpdateAgedBrieItem_UpdateItem_QualityIncreaseByOne(int sellIn, int quality)
         {
             var item = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.AgedBrie };
@@ -79,6 +104,8 @@ namespace GildedRose.Tests
         [InlineData(10, 30)]
         [InlineData(9, 50)]
         [InlineData(6, 0)]
+        [InlineData(0, 0)]
+        [InlineData(0, 10)]
         public async Task UpdateLegendaryItemTest_UpdateItem_QualityRemainTheSame(int sellIn, int quality)
         {
             var item = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.Legendary };
@@ -94,6 +121,20 @@ namespace GildedRose.Tests
             var item = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.Conjured };
             await _inventoryUpdater.Update(item);
             var expQuality = Math.Max(quality - 2, 0);
+            var expSellIn = Math.Max(sellIn - 1, 0);
+
+            Assert.Equal(expQuality, item.Quality);
+            Assert.Equal(expSellIn, item.SellIn);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 20)]
+        public async Task UpdateConjuredItemSellInPassedTest_UpdateConjuredItem_QualityDecreaseByFour(int sellIn, int quality)
+        {
+            var item = new ItemForSell { SellIn = sellIn, Quality = quality, Type = ItemForSellType.Conjured };
+            await _inventoryUpdater.Update(item);
+            var expQuality = Math.Max(quality - 4, 0);
             var expSellIn = Math.Max(sellIn - 1, 0);
 
             Assert.Equal(expQuality, item.Quality);
